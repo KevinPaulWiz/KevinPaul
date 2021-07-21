33dadcc13e038934c61810e94008efd45ca96cba
kugo234 <kugocoskey@gmail.com>
2021-07-21 15:53:27 +0300

    i have added a file called Kugo and i have printed out my name

--- Kugo.php
+++ Kugo.php
@@ -2,10 +2,10 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Sample</title>
+	<title> Sample</title>
 </head>
 <body>
-Eyoo here we go
+<p> Eyoo here we go</p>
 <p> My name is Kugonza Joseph</p>
 </body>
 </html>
